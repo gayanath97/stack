b76030995d02abb7ef88d30e73e47376da76db1c
Gayanath Lakmevan Silva <gayanathlakmevansilva@gmail.com>
2024-01-15 22:02:52 +0530

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack stack = new Stack(5);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);  
+        stack.display();
+        System.out.println();
+        System.out.println("Popped item: " + stack.pop());
+        System.out.println("Popped item: " + stack.pop());
+        stack.display();
+        System.out.println();
+        System.out.println("Peeked item: " + stack.peek());
+        stack.display();
     }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class Stack {
+public class Stack {
+    private int maxSize; //size of stack array
+    private int[] stackData;
+    private int top; //top of stack
+    //-------------------------------------------------------------------------
+    public Stack(int s) {
+        this.stackData = new int[s];
+        this.maxSize = s;
+        this.top = -1;
+    }
+    public boolean isEmpty() {
+        if(this.top == -1) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public boolean isFull() {
+        if(this.top == this.maxSize - 1) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public void push(int item) {
+        if(this.isFull()) {
+            System.out.println("Stack is full!");
+            return;
+        }
+        this.top++;
+        this.stackData[this.top] = item;
+    }
+    public int pop() {
+        if(this.isEmpty()) {
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+        int item = this.stackData[this.top];
+        this.top--;
+        return item;
+    }
+    public int peek() {
+        if(this.isEmpty()) {
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+        return this.stackData[this.top];
+    }
+    public void display() {
+        if(this.isEmpty()) {
+            System.out.println("Stack is empty!");
+            return;
+        }
+        for(int i = this.top; i >= 0; i--) {
+            System.out.print(this.stackData[i] + " ");
+        }
+    }
 }
